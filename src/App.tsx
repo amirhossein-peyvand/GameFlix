8ecc4cf79ee6de2979beb341f9670dda6a87d168
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-11-30 13:29:25 -0800

    Building a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  const isDesktop = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"
+               "main"`,
+        lg: `"nav nav"                 // 1024px
+             "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+
+      {isDesktop && (
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      )}
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
