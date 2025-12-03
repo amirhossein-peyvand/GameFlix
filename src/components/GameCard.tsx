bf7d53ad3e03d5dc8f4248809bce15dcdacd1274
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-12-03 14:54:10 -0800

    improving GTA V card issue

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,26 +7,32 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
-  if (game.id == 3498)
-    return (
-      <Card.Root
-        borderRadius={10}
-        overflow="hidden"
-        marginTop="40px"
-        marginLeft="40px"
-      >
-        <Image src={game.background_image} />
-        <Card.Body>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-        </Card.Body>
-      </Card.Root>
-    );
-
+  // if (game.id == 3498)
+  //   return (
+  //     <Card.Root
+  //       borderRadius={10}
+  //       overflow="hidden"
+  //       marginTop="40px"
+  //       marginLeft="40px"
+  //     >
+  //       <Image src={game.background_image} />
+  //       <Card.Body>
+  //         <Heading fontSize="2xl">{game.name}</Heading>
+  //         <PlatformIconList
+  //           platforms={game.parent_platforms.map((p) => p.platform)}
+  //         />
+  //       </Card.Body>
+  //     </Card.Root>
+  //   );
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
+    <Card.Root
+      borderRadius={10}
+      overflow="hidden"
+      {...(game.id === 3498 && {
+        marginTop: "40px",
+        marginLeft: "40px",
+      })}
+    >
       <Image src={game.background_image} />
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
