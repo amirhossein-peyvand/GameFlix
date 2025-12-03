b4ad3e55cfa8bfa6ccdf16ee5601bb1627f6ba5d
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-12-03 14:45:22 -0800

    Fixing GTA V card issue

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import type { Game } from "@/hooks/useGames";
-import { Card, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
@@ -7,6 +7,24 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+  if (game.id == 3498)
+    return (
+      <Card.Root
+        borderRadius={10}
+        overflow="hidden"
+        marginTop="40px"
+        marginLeft="40px"
+      >
+        <Image src={game.background_image} />
+        <Card.Body>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+        </Card.Body>
+      </Card.Root>
+    );
+
   return (
     <Card.Root borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
