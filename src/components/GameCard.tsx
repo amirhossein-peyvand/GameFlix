4738ff9b3c3d8e6ba22bd338184a11791c1f0f2a
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-12-04 04:32:15 -0800

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,29 +2,13 @@ import type { Game } from "@/hooks/useGames";
 import { Card, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  // if (game.id == 3498)
-  //   return (
-  //     <Card.Root
-  //       borderRadius={10}
-  //       overflow="hidden"
-  //       marginTop="40px"
-  //       marginLeft="40px"
-  //     >
-  //       <Image src={game.background_image} />
-  //       <Card.Body>
-  //         <Heading fontSize="2xl">{game.name}</Heading>
-  //         <PlatformIconList
-  //           platforms={game.parent_platforms.map((p) => p.platform)}
-  //         />
-  //       </Card.Body>
-  //     </Card.Root>
-  //   );
   return (
     <Card.Root
       borderRadius={10}
@@ -34,7 +18,7 @@ const GameCard = ({ game }: Props) => {
         marginLeft: "40px",
       })}
     >
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <Card.Body>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
