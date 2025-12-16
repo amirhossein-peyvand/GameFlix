157745ca64409bcc47d1e135e9ecba97b73fb65e
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-12-16 06:10:02 -0800

    Refactor - create lookup hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import usePlatforms from "@/hooks/usePlatforms";
 import { Button, Menu, Portal } from "@chakra-ui/react";
 import type { Platform } from "@/hooks/usePlatforms";
+import usePlatform from "@/hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
@@ -9,9 +10,7 @@ interface Props {
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-// import useData from "./useData";
 import APIClient from "@/services/api-client";
+import platforms from "@/data/platforms";
 
 export interface Platform {
     id: number;
@@ -10,13 +10,12 @@ export interface Platform {
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents')
 
-// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
-
 const usePlatforms = () => 
     useQuery({
         queryKey: ['platforms'],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000,
+        initialData: platforms
     })
 
 export default usePlatforms;
