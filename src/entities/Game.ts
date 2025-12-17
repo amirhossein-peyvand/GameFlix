2786bcbdd44086c3b8c941e245f64d1aaa90a251
Amirhossein <amirhosseinpeyvand2020@gmail.com>
2025-12-17 13:48:21 +0330

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,13 +1,17 @@
+import type { Genre } from "./Genre";
 import type { Platform } from "./Platform";
+import type { Publisher } from "./Publisher";
 
 
 export interface Game {
   background_image: string;
   description_raw: string;
+  genres: Genre[]
   id: number;
   metacritic: number;
   name: string;
   parent_platforms: { platform: Platform; }[];
+  publishers: Publisher[];
   rating_top: number;
   slug: string;
 }
